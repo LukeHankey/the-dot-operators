ac190f3f562f1e740430b4a71f728b1e6cedee69
Nathan <nathanlloyd@protonmail.com>
2023-09-07 23:51:15 +0100

    When run locally did not work relative imports into client were a little messy moved them out into `__main__.py`.  `__main__.py` now passes in the tile list/generator into the `GameClient` as part of action.. should probabaly just be `**kwagrs`.  also defined some constants, screen dimensions etc.

--- jigsaw/__main__.py
+++ jigsaw/__main__.py
@@ -5,17 +5,33 @@ from os import listdir
 from os.path import join, split
 from random import choice
 
-from .client import GameClient
+from client import GameClient
+from tessellation import square_tiler, tile_splitter
+from utils import get_image
 
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+SCREEN_DIMENSIONS = (SCREEN_WIDTH, SCREEN_HEIGHT)
+
+MAX_OVERLAP = 0.25
+MIN_OVERLAP = 0.15
+DEFAULT_OVERLAP = 0.25
+
+MIN_TILE_NUMBER = 16
+DEFAULT_TILE_NUMBER = 36
+
+action = {"num_of_tiles": DEFAULT_TILE_NUMBER}
 path = join(split(__file__)[0], "images")
 filename = choice(
     [  # get a random image on each game run
         f"images/{filename}" for filename in listdir(path)
     ]
 )
+
 filename = join(split(__file__)[0], filename)
-action = {"num_of_tiles": 16}
-action["overlap"] = 0.25  # 25% is the default value with a minimum of 15%
-action["filename"] = join(split(__file__)[0], filename)
+action["image"] = get_image(filename, SCREEN_DIMENSIONS, action["num_of_tiles"])
+
+action["overlap"] = DEFAULT_OVERLAP
+action["tiles"] = tile_splitter(square_tiler, action["image"], action["num_of_tiles"])
+
 game = GameClient(action)
 game.mainloop()
