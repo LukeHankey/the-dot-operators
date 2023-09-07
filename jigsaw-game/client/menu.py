ca0016af52ec18504647027310580635b83991ee
bartoszkobylinski <bartosz.kobylinski@gmail.com>
2023-09-07 07:55:07 +0200

    Title: Refactor menu.py Message: Transitioned from class-based views to function-based views in menu.py for better clarity and modularity. Introduced utility functions to reduce code redundancy. Enhanced game selection view to offer difficulty options.

--- jigsaw-game/client/menu.py
+++ jigsaw-game/client/menu.py
@@ -16,59 +16,94 @@ pygame.display.set_caption('Game Menu')
 font = pygame.font.SysFont(None, 36)  # Default font, size 36
 
 
-class BaseView(pygame.Surface):
-    """Base view that acts as a template for all game views."""
-
-    def __init__(self, width, height):
-        """Initialize the base view with given dimensions."""
-        super().__init__((width, height))
-
-    def draw(self):
-        """Draw the view elements on the screen"""
-        pass
-
-    def handle_event(self, event):
-        """Handle the given element"""
-        pass
+def draw_button(surface, rect, text, button_color, text_color):
+    pygame.draw.rect(surface, button_color, rect)
+    txt = font.render(text, True, text_color)
+    txt_pos = (rect.x + rect.width // 2 - txt.get_width() // 2, rect.y + rect.height // 2 - txt.get_height() // 2)
+    surface.blit(txt, txt_pos)
 
 
-class GameSelectionView(BaseView):
-    """View for the selection screen."""
-
-    pass
-
-
-class MenuView(BaseView):
-    """Main menu view"""
-
-    def draw(self):
-        """Draw the menu elements on the screen."""
-        self.fill(WHITE)
-        pygame.draw.rect(self, RED, (100, 100, 200, 50))
-        game_selection_text = font.render("Game Selection", True, BLUE)
-        self.blit(game_selection_text, (
-            150 - game_selection_text.get_width() // 2 + 50, 125 - game_selection_text.get_height() // 2))
-
-    def handle_event(self, event):
-        """Handle menu interactions."""
-        if event.type == MOUSEBUTTONDOWN:
-            x, y = event.pos
-            if 100 < x < 300 and 100 < y < 150:
-                return GameSelectionView(WIDTH, HEIGHT)
-        return self
+def is_button_clicked(event, rect):
+    if event.type == MOUSEBUTTONDOWN:
+        return rect.collidepoint(event.pos)
+    return False
 
 
-current_view = MenuView(WIDTH, HEIGHT)
+# Views
+def game_selection_view():
+    screen.fill(WHITE)
 
-running = True
-while running:
+    easy_button = pygame.Rect(100, 100, 200, 50)
+    moderate_button = pygame.Rect(100, 200, 200, 50)
+    hard_button = pygame.Rect(100, 300, 200, 50)
+
+    draw_button(screen, easy_button, "Easy", RED, BLUE)
+    draw_button(screen, moderate_button, "Moderate", RED, BLUE)
+    draw_button(screen, hard_button, "Hard", RED, BLUE)
+
     for event in pygame.event.get():
         if event.type == QUIT:
-            running = False
-        current_view = current_view.handle_event(event)
+            return {"type": "quit"}
+        elif is_button_clicked(event, easy_button):
+            return {"type": "play_game", "difficulty": "easy"}
+        elif is_button_clicked(event, moderate_button):
+            return {"type": "play_game", "difficulty": "moderate"}
+        elif is_button_clicked(event, hard_button):
+            return {"type": "play_game", "difficulty": "hard"}
 
-    current_view.draw()
-    screen.blit(current_view, (0, 0))
+    return {"type": "game_selection"}
+
+
+def play_game(difficulty):
+    screen.fill(WHITE)
+    message = f"You choose {difficulty} mode!"
+    txt = font.render(message, True, BLUE)
+    screen.blit(txt, (WIDTH // 2 - txt.get_width() // 2, HEIGHT // 2 - txt.get_height() // 2))
     pygame.display.flip()
+    pygame.time.wait(2000)
+    return {"type": "menu"}
 
-pygame.quit()
+
+def menu_view():
+    screen.fill(WHITE)
+    game_selection_button = pygame.Rect(100, 100, 200, 50)
+    draw_button(screen, game_selection_button, "Game Selection", RED, BLUE)
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            return {"type": "quit"}
+        if is_button_clicked(event, game_selection_button):
+            return {"type": "game_selection"}
+
+    return {"type": "menu"}
+
+
+# Main loop
+def menu():
+    current_view = "menu"
+    current_difficulty = None
+    view_functions = {
+        "menu": menu_view,
+        "game_selection": game_selection_view,
+    }
+
+    while True:
+        if current_view == "play_game":
+            action = play_game(current_difficulty)
+        else:
+            action = view_functions[current_view]()
+
+        if action["type"] == "quit":
+            break
+        elif action["type"] == "play_game":
+            current_difficulty = action['difficulty']
+            current_view = action['type']
+        elif action["type"] in view_functions:
+            current_view = action["type"]
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+
+menu()
